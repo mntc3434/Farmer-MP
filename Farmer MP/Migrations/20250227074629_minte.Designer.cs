6a9770e9c9b02959b31ea327b12f363510d50d30
Mintesinot getu <148215622+mntc3434@users.noreply.github.com>
2026-01-05 23:34:02 +0300

    Update 20250227074629_minte.Designer.cs

--- Farmer MP/Migrations/20250227074629_minte.Designer.cs
+++ Farmer MP/Migrations/20250227074629_minte.Designer.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-.
+
 #nullable disable
 
 namespace Farmer_MP.Migrations
